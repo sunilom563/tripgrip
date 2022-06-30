4f0f02f052613bdc6b3bbcd3f2b7fc08e3696b97
sunilom563 <sunilbdeshanur@gmail.com>
2022-06-30 23:35:45 +0200

    created googlemap api key

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from "react";
 
-const App = () => {
+import { CssBaseline, Grid } from "@material-ui/core";
 
-    return(
-        <div>
-            <h1>Hello Sunil</h1>
-        </div>
-    )
-}
+
+
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+
+
+
+const App = () => {
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid  container spacing={3} style={{width:'100%'}}>
+          <Grid item xs={12} md={4} >
+                <List />
+          </Grid>
+          <Grid item xs={12} md={8} >
+                <Map />
+          </Grid>
+
+      </Grid >
+      
+    </>
+  );
+};
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,44 @@
 import React from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, Toolbar, Typography, InputBase, Box } from  '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import useStyles from './styles';
+
 
 
 const Header = () => {
+
+         const classes = useStyles();
          return(
-             <h1>Header</h1>
-              )
+              <AppBar position="static">
+                <Toolbar className={classes.toolbar}>
+                    <Typography variant="h5"  className={classes.title}>
+                        TravelGrip
+
+                    </Typography>
+                    <Box display='flex'>
+                       <Typography variant="h6"  className={classes.title}>
+                            Explore the destinations
+                       </Typography>
+                       {/* <Autocomplete> */}
+                           <div className={classes.search}>
+                               <div className={classes.searchIcon}>
+                                  <SearchIcon/>
+
+                               </div>
+
+                               <InputBase placeholder="search..." classes={{ root:classes.inputRoot, input:classes.inputInput}} />
+
+                           </div>
+                       {/* </Autocomplete> */}
+                    </Box>
+
+                </Toolbar>
+
+              </AppBar>
+            
+            
+            );
 
 }
 
